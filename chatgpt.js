119a614178a1958488539723f88c28b0baae2a39
bswiley <bswiley@gmail.com>
2023-06-20 15:32:22 -0500

    added chatgpt code

--- chatgpt.js
+++ chatgpt.js
@@ -1,4 +1,18 @@
 require('dotenv').config();
+const { Configuration, OpenAIApi } = require('openai');
+const openai = new OpenAIApi(new Configuration({
+    apiKey: process.env.API_Key
+}))
+
+openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{role: "user", content: "Can you describe a project using python (in 500 words)"}],
+    })
+.then(res=> {
+    console.log(res.data.choices)
+})
+
+
 
 console.log('Bearer ' + process.env.API_Key);
 
