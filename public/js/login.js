f26841c92c74fe6aa7f8b3b3499f0d2e2118ba0a
bswiley <bswiley@gmail.com>
2023-06-19 22:34:34 -0500

    made login javascript work with login.handlebars

--- public/js/login.js
+++ public/js/login.js
@@ -21,7 +21,33 @@ const loginFormHandler = async (event) => {
     }
   }
 };
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+  console.log('signup pushed');
+  
 
+  const username = document.querySelector('#username-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (username && email && password) {
+    const response = await fetch('/api/users/signup', {
+      method: 'POST',
+      body: JSON.stringify({ username, email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    console.log(response)
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert('Failed to sign up.');
+    }
+  }
+};
 document
   .querySelector('.login-form')
   .addEventListener('submit', loginFormHandler);
+
+  document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
