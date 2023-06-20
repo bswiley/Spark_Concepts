0e6446e6b7a9e994d8160c4f2bf667de2ce95d61
bswiley <bswiley@gmail.com>
2023-06-19 22:36:02 -0500

    made userRoutes work with login javascript to signup user

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -16,7 +16,6 @@ router.post('/', withAuth, async (req, res) => {
     res.status(400).json(err);
   }
 });
-
 router.post('/login', async (req, res) => {
   try {
     const userData = await User.findOne({ where: { email: req.body.email } });
@@ -48,6 +47,21 @@ router.post('/login', async (req, res) => {
     res.status(400).json(err);
   }
 });
+router.post('/signup', async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      
+      res.json({ user: userData, message: 'You are now logged in!' });
+    });
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 
 router.post('/logout', (req, res) => {
   if (req.session.logged_in) {
